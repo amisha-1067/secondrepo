ceb864a1cf24455a77e3548b1bb2306f39bd73b6
amisha-1067 <85560784+amisha-1067@users.noreply.github.com>
2023-06-24 15:40:55 +0530

    uploading first.js

---
+++ first.js
@@ -0,0 +1,11 @@
+let prices = [7, 1, 5, 3, 6, 4];
+let buy = prices[0];
+let profit = 0;
+for (let i = 0; i < prices.length; i++) {
+  if (buy > prices[i]) {
+    buy = prices[i];
+  } else if (profit < prices[i] - buy) {
+    profit = prices[i] - buy;
+  }
+}
+console.log(profit);
